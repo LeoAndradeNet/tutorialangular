ae8c3a3080a8ac565cdb99bc827fbe0b747c7086
Léo Andrade <contato@leoandrade.net>
2021-12-04 10:24:43 -0300

    Inicial

---
+++ src/app/users/user.model.ts
@@ -0,0 +1,51 @@
+
+
+  export interface User {
+      id: number;
+      email: string;
+      first_name: string;
+      last_name: string;
+      avatar: string;
+  }
+
+  export interface ResponseUsers {
+      page: number;
+      per_page: number;
+      total: number;
+      total_pages: number;
+      data: User[];
+  }
+
+//modelos para a criacao
+    export interface RequestCreate {
+        name: string;
+        job: string;
+    }
+
+
+    export interface ResponseCreate {
+        name: string;
+        job: string;
+        id: string;
+        createdAt: Date;
+    }
+
+//Modelos para o get User
+
+export interface ResponseUser{
+    data: User
+}
+
+//modelos para o update
+export interface RequestUpdate {
+    name: string;
+    job: string;
+}
+
+
+export interface ResponseUpdate {
+    updatedAt: any;
+    name: string;
+    job: string;
+    updateAt: Date;
+}
